286a1dc0f50d61e0a1d6131c01e9702c4f247cc6
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2022-02-07 21:24:51 +0200

    mui theme added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import React, { FC } from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { red } from "@mui/material/colors"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+})
+
+
 const IndexPage: FC = () => (
+	<ThemeProvider theme={theme}>
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
@@ -25,6 +37,7 @@ const IndexPage: FC = () => (
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
+		</ThemeProvider>
   </Layout>
 )
 
